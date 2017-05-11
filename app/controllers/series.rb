0fecd6168b3f4d07903adc0bf9b7a91b5754a922
Zachary Elliott <contact@zell.io>
2017-05-11 15:02:55 -0400

    Add pagination to series display

--- app/controllers/series.rb
+++ app/controllers/series.rb
@@ -12,13 +12,21 @@ namespace '/series' do
   get '/:id' do |id|
     param :id, Integer, required: true
 
+    param :offset, Integer, required: false
+    param :limit, Integer, required: false
+
+    offset = params["offset"] || 0
+    limit = params["limit"] || 24
+
     series = Testudo::Model::Series[id]
     desc = "Books in #{series.name}"
 
     erb :"series/id", locals: {
       title: desc,
       description: desc,
-      series: series
+      series: series,
+      limit: limit,
+      offset: offset
     }
   end
 end
