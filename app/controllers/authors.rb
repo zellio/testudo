a47a1997df33c53e48b0bcba3101371de848e4ac
Zachary Elliott <contact@zell.io>
2017-05-11 15:02:47 -0400

    Add pagination to author display

--- app/controllers/authors.rb
+++ app/controllers/authors.rb
@@ -10,13 +10,21 @@ namespace '/authors' do
   get '/:id' do |id|
     param :id, Integer, required: true
 
+    param :offset, Integer, required: false
+    param :limit, Integer, required: false
+
+    offset = params["offset"] || 0
+    limit = params["limit"] || 24
+
     author = Testudo::Model::Author[id]
     desc = "Books by #{author.name}"
 
     erb :"authors/id", locals: {
       title: desc,
       description: desc,
-      author: author
+      author: author,
+      limit: limit,
+      offset: offset
     }
   end
 end
