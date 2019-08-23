6ae621b576863e9ea1e2a004bd5adab34de44e44
Zachary Elliott <contact@zell.io>
2019-08-22 20:14:13 -0400

    Refactors javascript for the reader application

--- app/static/js/reader.js
+++ app/static/js/reader.js
@@ -1,62 +1,108 @@
+function is_function(fn) {
+    return !!(fn && fn.constructor && fn.call && fn.apply);
+}
+
+function suppress_event(event) {
+    is_function(event.preventDefault) && !event.preventDefault.passive && event.preventDefault();
+    event.returnValue = false;
+}
+
+var EpubReader = (function() {
+    function EpubReader() {
+        this.url = new URL(document.URL);
+        this.currentLocationCfi = this.url.hash.slice(1);
+        this.book = ePub(this.url.pathname + "/");
+        this.rendition = this.book.renderTo("display", {
+            width: "100%",
+            height: "100%",
+        });
+        this.display = null;
+    }
+
+    function prev_page(event) {
+        this.rendition.prev();
+    }
+
+    function next_page(event) {
+        this.rendition.next();
+    }
+
+    function draw(target) {
+        return this.display = this.rendition.display(target);
+    }
+
+    EpubReader.prototype = {
+        nextPage: next_page,
+        prevPage: prev_page,
+        draw: draw,
+    };
+
+    return EpubReader;
+}());
+
 function run() {
-    let url = new URL(document.URL),
-        currentLocationCfi = url.hash.slice(1),
-        book = ePub(url.pathname + "/"),
-        rendition = book.renderTo("display", { width: "100%", height: "100%"}),
-        display,
-        arrow_direction = {
-            rtl: { prev: "ArrowRight", next: "ArrowLeft" },
-            ltr: { prev: "ArrowLeft", next: "ArrowRight" },
-        };
+    const KBD_ARROW_DIRECTION = {
+        rtl: { prev: "ArrowRight", next: "ArrowLeft" },
+        ltr: { prev: "ArrowLeft", next: "ArrowRight" },
+    };
 
-    function silenceClick(e) {
-        e.preventDefault && e.preventDefault();
-        e.returnValue = false;
-    }
+    let epub_reader = new EpubReader();
 
-    function prevPage(e) {
-        rendition.prev();
-        silenceClick(e);
-    }
-
-    function nextPage(e) {
-        rendition.next();
-        silenceClick(e);
-    }
-
-    if (currentLocationCfi) {
-        display = rendition.display(currentLocationCfi);
-    } else {
-        display = rendition.display();
-    }
-
-    rendition.on("relocated", (location) => {
-        console.log(location);
-        let cfi = location.start.cfi;
-        currentLocationCfi = cfi;
-        window.location.hash = cfi;
-        history.pushState({}, '', "#" + cfi);
+    epub_reader.book.loaded.navigation.then((navigation) => {
+        $('nav').html($('<select/>', {
+            class: "custom-select",
+            html: navigation.toc.map((chapter) => {
+                return $('<option/>', {
+                    id: "toc-li-" + chapter.id,
+                    class: "toc-list-item",
+                    value: chapter.href,
+                    text: chapter.label,
+                });
+            }),
+            change: (event) => {
+                let target = decodeURIComponent(event.currentTarget.value);
+                epub_reader.draw(target);
+            },
+        }));
     });
 
-    rendition.on("rendered", (section) => {
-        console.log(section);
+    epub_reader.rendition.on("keydown", (event) => {
+        parent.$(parent.document).trigger($.Event('keydown', event));
     });
 
-    rendition.on("keydown", (e) => {
-        console.log("container event");
-        silenceClick(e);
-        parent.$(parent.document).trigger($.Event('keydown', e));
+    // rendition.on("relocated", (location) => {
+    //   console.log("rendition:relocated");
+    //   console.log(location);
+    //   let cfi = location.start.cfi;
+    //   currentLocationCfi = cfi;
+    //   window.location.hash = cfi;
+    //   history.pushState({}, '', "#" + cfi);
+    // });
+
+    $(".arrow").on("touchstart mousedown", (event) => {
+        let arrow_button = $(event.currentTarget);
+        arrow_button.css({'opacity': 1});
+        arrow_button.on("touchend mouseup", (event) => {
+            setTimeout(() => {arrow_button.css({'opacity': 0})}, 100);
+        });
     });
 
-    $("#prev").on("click", prevPage);
-    $("#next").on("click", nextPage);
+    $("#prev").on("click", epub_reader.prevPage.bind(epub_reader));
+    $("#next").on("click", epub_reader.nextPage.bind(epub_reader));
 
-    $(window).on("keydown", (e) => {
-        let direction = book.package.metadata.direction || "ltr";
+    $(window).on("keydown", (event) => {
+        let direction = epub_reader.book.package.metadata.direction || "ltr";
 
-        switch (e.key) {
-        case arrow_direction[direction].prev: prevPage(e); break;
-        case arrow_direction[direction].next: nextPage(e); break;
+        switch (event.key) {
+        case KBD_ARROW_DIRECTION[direction].prev: epub_reader.prevPage(event); break;
+        case KBD_ARROW_DIRECTION[direction].next: epub_reader.nextPage(event); break;
         }
     });
+
+    let currentLocationCfi = epub_reader.url.hash.slice(1);
+    if (currentLocationCfi) {
+        epub_reader.draw(currentLocationCfi);
+    } else {
+        epub_reader.draw();
+    }
 }
